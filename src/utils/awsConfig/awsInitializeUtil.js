e8936cdef8cf85b501df05cdecca5a75536b2ecc
ScorpIan555 <ian.daley23@gmail.com>
2019-07-23 17:57:01 -0400

    code cleanup

--- src/utils/awsConfig/awsInitializeUtil.js
+++ src/utils/awsConfig/awsInitializeUtil.js
@@ -1,29 +1,27 @@
 import config from './awsAmplifyConfig';
 
-const awsConfig = {
-    Auth: {
-        mandatorySignIn: false,
-        region: config.cognito.REGION,
-        userPoolId: config.cognito.USER_POOL_ID,
-        identityPoolId: config.cognito.IDENTITY_POOL_ID,
-        userPoolWebClientId: config.cognito.APP_CLIENT_ID
-      },
-      Storage: {
-        region: config.s3.REGION,
-        bucket: config.s3.BUCKET,
-        identityPoolId: config.cognito.IDENTITY_POOL_ID
-      },
-      API: {
-        endpoints: [
-          {
-            name: "gNewsNotes",
-            endpoint: config.apiGateway.URL,
-            region: config.apiGateway.REGION
-          },
-        ]
-      }
+const awsConfig = { 
+  Auth: {
+      mandatorySignIn: false,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Storage: {
+      region: config.s3.REGION,
+      bucket: config.s3.BUCKET,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+      endpoints: [
+        {
+          name: "gNewsNotes",
+          endpoint: config.apiGateway.URL,
+          region: config.apiGateway.REGION
+        },
+      ]
+    }
 }
   
 export { awsConfig };
-
-
